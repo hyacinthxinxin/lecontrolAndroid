02bc14d57679cbf7696eecc84a646c4964479143
hyacinthxinxin <fanxin0202@163.com>
2017-03-27 15:46:29 +0800

    tcp s

--- app/src/main/java/com/cs1119it/fanxin/lecontrol/FloorAndAreaActivity.java
+++ app/src/main/java/com/cs1119it/fanxin/lecontrol/FloorAndAreaActivity.java
@@ -17,6 +17,14 @@ import com.cs1119it.fanxin.lecontrol.model.Area;
 import com.cs1119it.fanxin.lecontrol.model.Building;
 import com.cs1119it.fanxin.lecontrol.model.Floor;
 import com.cs1119it.fanxin.lecontrol.unit.SocketManager;
+import com.koushikdutta.async.http.AsyncHttpClient;
+import com.koushikdutta.async.http.socketio.Acknowledge;
+import com.koushikdutta.async.http.socketio.ConnectCallback;
+import com.koushikdutta.async.http.socketio.EventCallback;
+import com.koushikdutta.async.http.socketio.JSONCallback;
+import com.koushikdutta.async.http.socketio.SocketIOClient;
+import com.koushikdutta.async.http.socketio.SocketIORequest;
+import com.koushikdutta.async.http.socketio.transport.SocketIOTransport;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -45,6 +53,8 @@ public class FloorAndAreaActivity extends AppCompatActivity {
         setTitle("乐控");
         initData();
         initView();
+
+
     }
 
 //    @Override
